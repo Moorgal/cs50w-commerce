b6a34d4fb8553e17208b5c09d3ebc3ca0813c23a
Peter Bokor <tumblepeete@gmail.com>
2023-07-26 22:31:08 +0100

    models update

--- auctions/models.py
+++ auctions/models.py
@@ -9,14 +9,42 @@ class Categories(models.Model):
     title = models.CharField(max_length=200)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
+    def __str__(self):
+        return self.title
+
 
 class Listing(models.Model):
     title = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    price = models.FloatField()
-    link = models.CharField(max_length=2000)
+    category = models.ManyToManyField('Categories', blank=True)
+    listing_price = models.FloatField(default=0, null=True, blank=True)
+    image_link = models.CharField(max_length=2000)
     is_available = models.BooleanField(default=True)
+    bid_amount = models.IntegerField(default=0, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
-    category = models.ForeignKey(Categories, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+    
+
+#class Bids(models.Model):
+   # id
+   # listing_id
+   # price
+   # date
+   # user
+
+
+#class watchlist
+#id
+#user
+#listing_id
+
+#class comments
+##id
+#listing_id
+#User
+#body
+#date
